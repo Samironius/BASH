675bc04daab90a3e875c71b94a90f6f4407b77f3
samironius <samirshahubs@gmail.com>
2021-08-23 02:31:17 +0300

    Assignment for regex

---
+++ Scripting-3/sc.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+read surname
+
+if [[ "$surname" =~ ^shahub@* ]]; then
+	echo "Surname is $surname"
+else
+	echo "Error"
+fi
+
+read name
+
+if [[ "$name" =~ ^samir@* ]]; then
+	echo "Name is $name"
+else
+	echo "Error"
+fi
+
+read num
+if [[ "$num" =~ ^380@* && ${#num} =~ 12 ]]; then
+	echo "Number is $num"
+else
+	echo "Error"
+fi
+
+cat /etc/passwd | grep -E "bash"
+cat /etc/group | grep -E "^daemon"
+cat /etc/group | grep -v "^daemon"
+sudo find /etc -name README | wc -l
+grep -l -d skip argparse * | find * -mmin -600
+
